f7935ed2015992cd7a6eae905d353a77dbb72b12
Daniel Nettleton <daniel@theartfulcoder.com>
2015-07-15 16:46:45 +0000

    Added user's location map

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Fazenda Sorter (Alpha)</title>
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true">
+        <script src="geolocate.js">
+    </head>
+    <body>
+        <h1>Sort Test</h1>
+        <h2>User Location:</h2>
+        <div id="user-map-canvas"></div>
+        <p>List of farms:</p>
+        <ul>
+            <li>Chimaltenango, Guatemala (14.678053, -90.884475)</li>
+            <li>Ruiru, Kenya (-1.145770, 36.979925)</li>
+            <li>Narino, Columbia (1.292461, -77.349312)</li>
+        </ul>
+    </body>
+</html>
